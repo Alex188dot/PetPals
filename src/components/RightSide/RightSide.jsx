2980b9fcc175b53854f2d2b9841e1db6d5146d5a
alessio.leodori <alessio.leodori@gmail.com>
2023-10-18 15:06:48 +0200

    added TrendCard

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,6 +4,7 @@ import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
 
 const RightSide = () => {
   return (
@@ -14,6 +15,8 @@ const RightSide = () => {
         <img src={Noti} alt="notification" />
         <img src={Comment} alt="comment" />
       </div>
+      <TrendCard />
+      <button className="button r-button">Share</button>
     </div>
   );
 };
