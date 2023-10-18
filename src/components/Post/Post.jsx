04cada33e0b8a4eafa5712b32617ebfcd41a5d53
alessio.leodori <alessio.leodori@gmail.com>
2023-10-18 10:54:22 +0200

    added homefeed

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Post.css";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
+
+const Post = ({ data }) => {
+  return (
+    <div className="Post">
+      <img className="postImage" src={data.img} alt="post_image" />
+      <div className="postReactions desc">
+        <img src={data.liked ? Heart : NotLike} alt="heart-icon" />
+        <img src={Comment} alt="comment-icon" />
+        <img src={Share} alt="share-icon" />
+      </div>
+      <span
+        className="desc"
+        style={{ color: "var(--gray)", fontSize: "0.8rem" }}
+      >
+        {data.likes} likes
+      </span>
+      <div className="detail desc">
+        <div className="fullname">
+          <b>{data.name}</b>
+        </div>
+        <br />
+        <span> {data.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
