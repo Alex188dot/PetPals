b405c885a602d2008afe1b299abf0ed2f42048e3
alessio.leodori <alessio.leodori@gmail.com>
2023-10-19 09:54:28 +0200

    added Share Modal

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./RightSide.css";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
+import PostShare from "../PostShare/PostShare";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
 
 const RightSide = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <div className="RightSide">
       <div className="navIcons">
@@ -16,9 +21,31 @@ const RightSide = () => {
         <img src={Comment} alt="comment" />
       </div>
       <TrendCard />
-      <button className="button r-button">Share</button>
+      <Button className="button r-button" onClick={open}>
+        Share
+      </Button>{" "}
+      <ShareModal open={open} opened={opened} close={close} />
     </div>
   );
 };
+
+function ShareModal({ open, opened, close }) {
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Edit your info"
+        size="40%"
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+      >
+        <PostShare />
+      </Modal>
+    </>
+  );
+}
 
 export default RightSide;
