b649ff1a0fc31cae7e3490ddfcf8134c33d3db29
alessio.leodori <alessio.leodori@gmail.com>
2023-10-19 08:42:26 +0200

    added sign up form

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,12 +1,82 @@
 import React from "react";
 import "./Auth.css";
-
+import Logo from "../../components/Logo/Logo";
 const Auth = () => {
   return (
     <div className="Auth">
-      <div className="a-left">Auth</div>
+      <div className="a-left">
+        <div className="auth-logo">
+          <Logo />
+        </div>
+        <div className="Webname">
+          <h1>Pet Pals</h1>
+          <h6>
+            The social media app for pet lovers to find and share pet-related
+            content.
+          </h6>
+        </div>
+      </div>
+      <SignUp />
     </div>
   );
 };
+
+function SignUp() {
+  return (
+    <div className="a-right">
+      <form action="" className="infoForm authForm">
+        <h3>Sign up</h3>
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            id=""
+            placeholder="First Name"
+            className="infoInput"
+          />
+          <input
+            type="text"
+            name="lastName"
+            id=""
+            placeholder="Last Name"
+            className="infoInput"
+          />
+          <div>
+            <input
+              type="text"
+              name="username"
+              id=""
+              placeholder="Username"
+              className="infoInput"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Password"
+              className="infoInput"
+            />
+            <input
+              type="password"
+              name="confirmPassword"
+              id=""
+              placeholder="Confirm password"
+              className="infoInput"
+            />
+          </div>
+        </div>
+        <div>
+          <span style={{ fontSize: "0.8rem" }}>
+            Already have an account? Login
+          </span>
+        </div>
+        <bytton className="button infoButton">Sign Up</bytton>
+      </form>
+    </div>
+  );
+}
 
 export default Auth;
