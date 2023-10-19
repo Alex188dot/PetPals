cdb12577ef9c40f96b9042ac6cc7414f942a8d62
alessio.leodori <alessio.leodori@gmail.com>
2023-10-19 09:38:24 +0200

    added edit info modal

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
 
 const InfoCard = () => {
   return (
@@ -8,7 +10,7 @@ const InfoCard = () => {
       <div className="infoHead">
         <h4>Your info</h4>
         <div>
-          <UilPen width="2rem" height="1.1rem" />
+          <ProfileModal />
         </div>
       </div>
       <div className="info">
@@ -35,5 +37,94 @@ const InfoCard = () => {
     </div>
   );
 };
+
+function ProfileModal() {
+  const [opened, { open, close }] = useDisclosure(false);
+  const buttonRef = useRef();
+
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Edit your info"
+        size="40%"
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+      >
+        <form className="infoForm">
+          <h3>Your info</h3>
+          <div>
+            <input
+              className="infoInput"
+              type="text"
+              name="firstName"
+              id=""
+              placeholder="First Name"
+              style={{ marginRight: "1rem" }}
+            />
+            <input
+              className="infoInput"
+              type="text"
+              name="lastName"
+              id=""
+              placeholder="Last Name"
+              style={{ marginLeft: "1rem" }}
+            />
+          </div>
+          <div>
+            <input
+              className="infoInput"
+              type="text"
+              name="pet"
+              id=""
+              placeholder="Pet"
+              style={{ marginTop: "-1rem", width: "100%" }}
+            />
+          </div>
+          <div>
+            <input
+              className="infoInput"
+              type="text"
+              name="from"
+              id=""
+              placeholder="From"
+              style={{ marginRight: "1rem", marginTop: "-1rem" }}
+            />
+            <input
+              className="infoInput"
+              type="text"
+              name="memberSince"
+              id=""
+              placeholder="Member since"
+              style={{ marginLeft: "1rem", marginTop: "-1rem" }}
+            />
+          </div>
+          <div>
+            <span style={{ marginRight: "1rem" }}>Profile Image</span>
+            <input type="file" name="profileImg" id="" />
+          </div>
+          <div>
+            <span style={{ marginRight: "1rem" }}>Cover Image</span>
+            <input type="file" name="coverImg" id="" />
+          </div>
+          <button className="button infoButton">Update Info</button>
+        </form>
+      </Modal>
+      <Button
+        style={{ display: "none" }}
+        onClick={open}
+        ref={buttonRef}
+      ></Button>{" "}
+      <UilPen
+        width="2rem"
+        height="1.1rem"
+        onClick={() => buttonRef.current.click()}
+      />
+    </>
+  );
+}
 
 export default InfoCard;
