f8e763122f36897e04b5416183e30732aa163b4f
alessio.leodori <alessio.leodori@gmail.com>
2023-10-20 10:23:28 +0200

    added login

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,9 +1,18 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req, res) => {
   const { username, password, firstName, lastName } = req.body;
 
-  const newUser = new UserModel({ username, password, firstName, lastName });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new UserModel({
+    username,
+    password: hashedPassword,
+    firstName,
+    lastName,
+  });
 
   try {
     await newUser.save();
@@ -11,4 +20,22 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+// Login User
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      validity
+        ? res.status(200).json(user)
+        : res.status(400).json("Wrong username or password");
+    } else {
+      res.status(400).json("User does not exist");
+    }
+  } catch (error) {}
 };
