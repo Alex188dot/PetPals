c0a32bd705e31d64a0676397f8df560b0edab42b
alessio.leodori <alessio.leodori@gmail.com>
2023-10-22 16:00:53 +0200

    added update user

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 // Get a user from DB
 
@@ -17,5 +18,28 @@ export const getUser = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json(error);
+  }
+};
+
+// update a user
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus, password } = req.body;
+
+  if (id === currentUserId || currentUserAdminStatus) {
+    try {
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("Access denied! You can only udate your own profile");
   }
 };
