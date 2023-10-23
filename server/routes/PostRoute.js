2af11c6f874026f5b4339aff2651aab1eb9e45b9
alessio.leodori <alessio.leodori@gmail.com>
2023-10-23 10:55:44 +0200

    added timeline posts

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -5,6 +5,7 @@ import {
   updatePost,
   deletePost,
   likePost,
+  getTimelinePosts,
 } from "../controllers/PostController.js";
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.get("/:id", getPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
 router.put("/:id/like", likePost);
+router.get("/:id/timeline", getTimelinePosts);
 
 export default router;
