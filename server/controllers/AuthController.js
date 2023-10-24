110ce402de14569487acbf472b8e9a20b2912c22
alessio.leodori <alessio.leodori@gmail.com>
2023-10-24 15:05:19 +0200

    implemented jwt

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,21 +1,34 @@
 import UserModel from "../models/UserModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  const { username, password, firstName, lastName } = req.body;
-
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const newUser = new UserModel({
-    username,
-    password: hashedPassword,
-    firstName,
-    lastName,
-  });
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+  const { username } = req.body;
+  const newUser = new UserModel(req.body);
 
   try {
-    await newUser.save();
+    const oldUser = await UserModel.findOne({ username });
+    if (oldUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const user = await newUser.save();
+    const token = jwt.sign(
+      {
+        username: user.username,
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({
+      user,
+      token,
+    });
     res.status(200).json(newUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -31,9 +44,24 @@ export const loginUser = async (req, res) => {
     const user = await UserModel.findOne({ username: username });
     if (user) {
       const validity = await bcrypt.compare(password, user.password);
-      validity
-        ? res.status(200).json(user)
-        : res.status(400).json("Wrong username or password");
+      if (!validity) {
+        res.status(400).json("Wrong password");
+      } else {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            id: user._id,
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: "1h",
+          }
+        );
+        res.status(200).json({
+          user,
+          token,
+        });
+      }
     } else {
       res.status(400).json("User does not exist");
     }
