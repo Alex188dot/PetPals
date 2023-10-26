d0f8e974ccd02fc8800db40d2035dc7895b5c02c
alessio.leodori <alessio.leodori@gmail.com>
2023-10-26 09:52:56 +0200

    implemented routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./pages/auth/Auth";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
+
   return (
     <div className="App">
-      <Home />
-      {/* <Profile /> */}
-      {/* <Auth /> */}
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/home" /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="/home" /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
