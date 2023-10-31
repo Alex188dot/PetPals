f44494dc823c68fbb0579c7fceebc93fb7bc77bd
alessio.leodori <alessio.leodori@gmail.com>
2023-10-31 13:05:49 +0100

    fixed own posts in profile section

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -3,21 +3,26 @@ import "./Posts.css";
 import Post from "../Post/Post";
 import { useDispatch, useSelector } from "react-redux";
 import { getTimelinePosts } from "../../actions/postAction";
+import { useParams } from "react-router-dom";
 
 const Posts = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.authReducer.authData);
-  const { posts, loading } = useSelector((state) => state.postReducer);
+  let { posts, loading } = useSelector((state) => state.postReducer);
+  const params = useParams();
 
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
+
+  if (!posts) return "No posts to show";
+  if (params.id) posts = posts.filter((post) => post.userId === params.id);
   return (
     <div className="Posts">
       {loading
         ? "Fetching Posts..."
         : posts.map((post, id) => {
-            return <Post data={post} id={id} />;
+            return <Post key={id} data={post} id={id} />;
           })}
     </div>
   );
